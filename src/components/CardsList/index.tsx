0efc82ba2ced1a0440b776ca8ee2253846cf027b
thix <thierrysales2001@gmail.com>
2024-10-07 17:18:24 -0300

    novo

--- src/components/CardsList/index.tsx
+++ src/components/CardsList/index.tsx
@@ -38,7 +38,7 @@ export const CardsList = () => {
 			/>
 
 			<SuccessModal
-				checkoutUrl="/" // colocar url de pagamento aqui
+				checkoutUrl="https://bumbum-git-main-thixs-projects.vercel.app/?__vercel_draft=1" // colocar url de pagamento aqui
 				isOpen={isModalOpen}
 				onClose={handleToggleModal}
 			/>
